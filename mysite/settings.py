4a60dc69227431116d5440db8bff54804777dc42
Aaron Lin <aaron.tlin@gmail.com>
2020-08-05 18:33:14 -0400

    removed celery

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,13 +136,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),    
 )
 STATIC_ROOT='staticfiles'
-# CELERY STUFF
-BROKER_URL = 'django://'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-#CELERY_TIMEZONE = 'Africa/Nairobi'
+
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
